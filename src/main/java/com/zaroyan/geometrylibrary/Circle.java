f73b985b64f695b7a60c5f8e6286ee16ef20a98e
Albert Zaroyan <albertzaroyan@yandex.ru>
2024-06-03 17:41:51 +0300

    "созданы классы геометрических фигур"

---
+++ src/main/java/com/zaroyan/geometrylibrary/Circle.java
@@ -0,0 +1,21 @@
+package com.zaroyan.geometrylibrary;
+
+/**
+ * @author Zaroyan
+ */
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
